95f9f9dfc531cfaec38118b660c062ea745f55b9
kinvamp <sode44vee@gmail.com>
2024-10-02 20:53:35 +0800

    Add files via upload

---
+++ website/script.js
@@ -0,0 +1,14 @@
+// Smooth scrolling to sections
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
